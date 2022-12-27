760ef8b31891a8ce2020742ed850e013d61aa8d4
Xavier Salazar <xavierssalazar92@gmail.com>
2022-12-26 23:00:30 -0600

    updating sql statements

---
+++ db/units.sql
@@ -0,0 +1,33 @@
+-- -------------------------------------------------------------
+-- Units Data
+--
+-- Database: postgres
+-- -------------------------------------------------------------
+
+INSERT INTO "Unit" ("id", "name") VALUES
+(1, '10 Minutes'),
+(2, '15 Minutes'),
+(3, '20 Minutes'),
+(4, '30 Minutes'),
+(5, '35 Minutes'),
+(6, '45 Minutes'),
+(7, '50 Minutes'),
+(8, '1 Hour'),
+(9, '1.5 Hours'),
+(10, '1 Month'),
+(11, '6 Months'),
+(12, 'Add to any 1 Hour Massage'),
+(13, 'Add to any Facial'),
+(14, 'Add to any session'),
+(15, 'Add to any Swedish or Deep Tissue Massage'),
+(16, 'Extra bowl'),
+(17, 'Per Minute'),
+(18, 'Per Hour'),
+(19, 'Per Person'),
+(20, 'Class Session'),
+(21, 'Private Session'),
+(22, 'Range'),
+(23, 'Upgrade to Deep Tissue'),
+(24, '25 Minutes'),
+(25, 'Manicure'),
+(26, 'Pedicure');
